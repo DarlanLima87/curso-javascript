57008603fe54cd73c1f8a7265d5f9edf6ca51da4
Darlan Lima <99288280+DarlanLima87@users.noreply.github.com>
2024-01-10 17:04:45 -0300

    Exercicio
    Tabuada

--- Aula14ex/ex17/scrpit.js
+++ Aula14ex/ex17/scrpit.js
@@ -7,13 +7,19 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] FALTAM DADOS!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if (i < f)
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
-        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }    
+        res.innerHTML += `${c} \u{1F3C1}`
     }
 }
